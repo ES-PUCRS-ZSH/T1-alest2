9013a863ab01bc0570964c98712eaa7e8b09d8bf
João Vitor <joao.vitorms1509@gmail.com>
2025-03-07 01:39:39 -0300

    Feat: Leitura dos casos de teste

--- src/main/kotlin/edu/pucrs/zsh/Main.kt
+++ src/main/kotlin/edu/pucrs/zsh/Main.kt
@@ -1,5 +1,18 @@
 package edu.pucrs.zsh
 
+
+val leitor = TextReader()
+
+
+
 fun main (){
-	print("Hello World!")
+	println("Hello World!")
+//	println("Digite o nome do arquivo: ")
+//	val nome: String = readln()
+
+	val (primeiraLinha, segundaLinha) = leitor.lerTexto("casoteste")
+
+	println("Numero do array: $primeiraLinha")
+	println("Array: $segundaLinha")
+
 }
---
+++ src/main/kotlin/edu/pucrs/zsh/TextReader.kt
@@ -0,0 +1,26 @@
+package edu.pucrs.zsh
+import java.io.BufferedReader
+import java.io.File
+
+class TextReader {
+	fun lerTexto (name: String): Pair<String, ArrayList<Int>> {
+		val inputString = File("src/main/resources/$name.txt")
+		val reader = inputString.bufferedReader()
+		val primeiraLinha = reader.readLine()
+		val segundaLinha = reader.readLines()
+
+		val lista = ArrayList<String>()
+		val numeros = ArrayList<Int>()
+
+		for(line in segundaLinha) {
+			val numerosString = line.split(" ")
+
+			for(numero in numerosString){
+				numeros.add(numero.toInt())
+			}
+
+		}
+
+		return Pair(primeiraLinha, numeros)
+	}
+}
